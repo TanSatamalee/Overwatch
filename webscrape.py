6b72a839272f9a6d29a35c03810204bac0e4ca13
TanSatamalee <tanitan14@berkeley.edu>
2017-06-14 21:36:38 -0700

    added functions for getting overall stats

--- webscrape.py
+++ webscrape.py
@@ -5,11 +5,40 @@ url = 'https://playoverwatch.com/en-us/career/pc/us/'
 
 my_ign = 'tannooby-11963'
 
-page = requests.get(url + my_ign)
 
-soup = BeautifulSoup(page.content, 'html.parser')
+# Returns BeautifulSoup object given a player's name
+def createSoup(player):
+	page = requests.get(url + player)
+	return BeautifulSoup(page.content, 'html.parser')
 
-temp = soup.find_all(class_='card-heading')
+# Returns the overall player stats for a specific mode('qp' or 'comp')
+def get_overall_stats(player, mode):
+	# Checks for valid player argument
+	if player is None:
+		print('Not a valid player name.')
+		return None
 
-for a in temp:
-	print(a.get_text())
+	soup = createSoup(player)
+	featured_stats = soup.find_all(class_='card-heading')
+	l = int(len(featured_stats) / 2)
+	stats = []
+	if mode == 'qp':
+		for fs in featured_stats[:l]:
+			stats.append(fs.get_text())
+	elif mode == 'comp':
+		for fs in featured_stats[l:]:
+			stats.append(fs.get_text())
+	else:
+		print('Not a valid mode.')
+		return None
+	return stats
+
+# Returns the label for the overall stats of a player
+def get_overall_stats_label():
+	soup = createSoup(my_ign)
+	featured_stats = soup.find_all(class_='card-copy')
+	stats = []
+	for fs in featured_stats:
+		stats.append(fs.get_text())
+	return stats[:int(len(stats) / 2)]
+
