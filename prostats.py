0b0eb7d536e4677e97350bb9c72d6c64e99f9a03
TanSatamalee <tanitan14@berkeley.edu>
2017-06-19 19:57:55 -0700

    added scrolling, need better way to scroll

--- prostats.py
+++ prostats.py
@@ -1,11 +1,25 @@
 import webscrape as ws
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 from bs4 import BeautifulSoup
 
 # Returns a list of the top 500 players with their id.
 def get_leaderboard():
 	url = 'https://masteroverwatch.com/leaderboards/pc/global'
-	page = ws.request_page(url)
-	soup = BeautifulSoup(page.content, 'html.parser')
+	browser = webdriver.Chrome("C:/Games/chromedriver.exe")
+	browser.get(url)
+	time.sleep(1)
+
+	elem = browser.find_element_by_tag_name("body")
+
+	pagedowns = 100
+	while pagedowns:
+		elem.send_keys(Keys.PAGE_DOWN)
+		time.sleep(1)
+		pagedowns = pagedowns - 1
+
+	soup = BeautifulSoup(browser.page_source, 'html.parser')
 	links = soup.find_all('a', {'class':'table-row-link'}, href=True)
 
 	players = []
