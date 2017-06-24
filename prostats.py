1d4e440e18b07adbf5225267ccf4a9b8dd1031db
TanSatamalee <tanitan14@berkeley.edu>
2017-06-23 18:59:40 -0700

    added combat stats

--- prostats.py
+++ prostats.py
@@ -29,9 +29,12 @@ def get_leaderboard():
 	return players
 
 
-# Returns the popularity and winrate of all heroes for a specified mode.
-def get_hero_overview(mode):
-	url = 'https://masteroverwatch.com/heroes/pc/us/mode/'
+# Returns the overview stats of all heroes for a specified mode.
+# Stats: Popularity, Winrate, KDA, Medals
+# Locations: global, us, eu, kr, cn
+# Mode: qp, comp
+def get_hero_overview(location, mode):
+	url = 'https://masteroverwatch.com/heroes/pc/' + location + '/mode/'
 	if mode == 'qp':
 		url = url + 'quick'
 	elif mode == 'comp':
@@ -48,14 +51,57 @@ def get_hero_overview(mode):
 	hero_name = []
 	popularity = []
 	winrate = []
+	kda = []
+	medals = []
 	for h in heroes:
 		hero_name.append(h.find('strong').getText())
 		t = h.find_all(class_='bar-outer')
 		popularity.append(t[0].getText())
 		winrate.append(t[1].getText())
+		kda.append(t[2].getText())
+		medals.append(t[3].getText())
 
-	result = [hero_name, popularity, winrate]
+	result = [hero_name, popularity, winrate, kda, medals]
 
 	return result
 
-get_hero_overview('qp')
+
+# Returns the combat stats of all heroes for a specified mode.
+# Stats: elims, deaths, damage, block, heals, accuracy
+# Locations: global, us, eu, kr, cn
+# Mode: qp, comp
+def get_hero_combat(location, mode):
+	url = 'https://masteroverwatch.com/heroes/pc/' + location + '/mode/'
+	if mode == 'qp':
+		url = url + 'quick'
+	elif mode == 'comp':
+		url = url + 'ranked'
+	else:
+		return None
+
+	page = ws.request_page(url)
+	soup = BeautifulSoup(page.content, 'html.parser')
+
+	table = soup.find_all('div', class_='tab-pane')
+	heroes = table[1].find('div', class_='table-body').find_all(class_='table-row row')
+	
+	hero_name = []
+	elim = []
+	death = []
+	damage = []
+	block = []
+	heal = []
+	acc = []
+	for h in heroes:
+		hero_name.append(h.find('strong').getText())
+		t = h.find_all(class_='bar-outer')
+		elim.append(t[0].getText())
+		death.append(t[1].getText())
+		damage.append(t[2].getText())
+		block.append(t[3].getText())
+		heal.append(t[4].getText())
+		acc.append(t[5].getText())
+
+	result = [hero_name, elim, death, damage, block, heal, acc]
+
+	return result
