d9c3bff6035440026e095b330e04ff111d8160f1
TanSatamalee <tanitan14@berkeley.edu>
2017-07-21 21:43:58 -0700

    added player stats storage function with sqlite

--- logstats.py
+++ logstats.py
@@ -1,11 +1,34 @@
 import sqlite3
 import datetime
+import re
 import globalstats as gs
+import playerstats as ps
 from pathlib import Path
-
 
 locations = ['global','us','eu','kr','cn']
 modes = ['qp','comp']
+
+
+# Gets rid of all non-alphanumeric characters of a string.
+def _convert_string(s):
+    if str.isdigit(s[0]):
+        return re.sub('[^0-9]+', '', s)
+    return re.sub('[^0-9a-zA-Z]+', '', s)
+
+# Prints the column names for a table conn.
+def _get_table_column_keys(conn):
+    cursor = conn.execute('select * from globalstats')
+    names = list(map(lambda x: x[0], cursor.description))
+    print(names)
+
+# Prints tables for specific database name.
+def _print_database(name, table):
+    conn = sqlite3.connect(name)
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM ' + table)
+    for a in cur.fetchall():
+        print(a)
+
 
 # Saves global hero stats for all locations and modes in files.
 # File naming: mode_location_globalstats.db
@@ -37,17 +60,32 @@ def store_global_stats():
             conn.commit()
             conn.close()
 
+# Saves player sats for all modes for a specific player.
+# Player: username-battlenetid
+# Folder: name of folder to put database file into.
+def store_player_stats(player, folder):
+    now = datetime.datetime.now()
+    date = str(now.month) + str(now.day) + str(now.year)
+    global modes
+    for m in modes:
+        data = ps.get_hero_stats(player, m)
+        for d in data:
+            hero = _convert_string(d['hero'])
+            conn = sqlite3.connect(folder + '/' + m + '_playerstats.db')
 
-# Prints the column names for a table conn.
-def _get_table_column_keys(conn):
-    cursor = conn.execute('select * from globalstats')
-    names = list(map(lambda x: x[0], cursor.description))
-    print(names)
+            create_table = 'CREATE TABLE IF NOT EXISTS ' + hero \
+                + ' (date TEXT PRIMARY KEY'
+            for label in d:
+                if not label == 'hero':
+                    create_table += ', ' + _convert_string(label) + ' REAL'
+            create_table += ');'
+            conn.execute(create_table)
 
-# Prints tables for specific database name.
-def _print_database(name):
-    conn = sqlite3.connect(name)
-    cur = conn.cursor()
-    cur.execute('SELECT * FROM globalstats')
-    for a in cur.fetchall():
-        print(a)
+            insert_table = 'INSERT INTO ' + hero + ' VALUES(\'' + date + '\''
+            for k, v in d.items():
+                if not k == 'hero':
+                    insert_table +=  ', ' + str(_convert_string(v))
+            conn.execute(insert_table + ' )')
+
+            conn.commit()
+            conn.close()
